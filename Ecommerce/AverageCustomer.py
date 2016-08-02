0af27a0a633e61c9d5b11dcea9829b332aa9bc80
Kim <daniel.kim1116@gmail.com>
2016-08-02 10:40:20 -0700

    MapReduce step functions done

--- Ecommerce/AverageCustomer.py
+++ Ecommerce/AverageCustomer.py
@@ -1,6 +1,27 @@
-    def mapper_make_counts_key(self, word, count):
-        yield '%04d'%int(count), word
+from mrjob.job import MRJob
+from mrjob.step import MRStep
 
-    def reducer_output_words(self, count, words):
-        for word in words:
-            yield count, word
+class SpendByCustomerSorted(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_orders,
+                   reducer=self.reducer_totals_by_customer),
+            MRStep(mapper=self.mapper_make_amounts_key,
+                   reducer=self.reducer_output_results)
+        ]
+    def mapper_get_orders(self, _, line):
+        (customerID, itemID, orderAmount) = line.split(',')
+        yield customerID, float(orderAmount)
+
+    def reducer_totals_by_customer(self, customerID, orders):
+        yield customerID, sum(orders)
+
+    def mapper_make_amounts_key(self, customerID, orderTotal):
+        yield '%04.02f'%float(orderTotal), customerID
+        
+    def reducer_output_results(self, orderTotal, customerIDs):
+        for customerID in customerIDs:
+            yield customerID, orderTotal
+
+if __name__ == '__main__':
+    SpendByCustomerSorted.run()
